c21eac5ba5f26e3aded481282c998296083fa41a
Md Tabraiz <138916881+tabraiz06@users.noreply.github.com>
2023-10-01 12:44:52 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,37 @@
+let string=''
+function pressed(v){
+    
+    
+       let oprSym=['+','-','*','/','.']   
+       let t=string.length;
+       let lastCHR=string[t-1]
+       console.log(lastCHR)
+       if(oprSym.includes(v)){
+          if(oprSym.includes(lastCHR)){
+            let newstr=string.slice(0,-1)
+            console.log(newstr)
+           string= newstr+v
+           document.querySelector('.input-holder').innerHTML=string
+          }
+          else{
+            string+=v
+            document.querySelector('.input-holder').innerHTML=string
+          }
+       } 
+       else {
+        string+=v
+        document.querySelector('.input-holder').innerHTML=string
+       }    
+   
+
+}
+function cal(){
+   
+    string=eval(string)
+    document.querySelector('.input-holder').innerHTML=string
+}
+function clean(){
+   
+    string=''
+            document.querySelector('.input-holder').innerHTML=string
+}
